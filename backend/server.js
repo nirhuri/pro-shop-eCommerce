e5d6c0fe0b2bc2b22ea19b359f8836b716b2a5fd
nirhuri <hurinir@gmail.com>
2021-09-01 23:59:04 +0300

    Adding custom error middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,28 @@
-const express = require('express');
+import express from 'express';
+import dotenv from "dotenv";
+import colors from 'colors';
 
-const products = require('./data/products');
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+import connectDB from './config/db.js';
+
+import productRoutes from './routes/productRoutes.js';
+
+dotenv.config();
 
 const app = express();
+connectDB();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
+app.use(notFound);
 
-    res.json(product);
-});
+app.use(errorHandler);
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
