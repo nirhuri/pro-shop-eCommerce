b6415f1cfde010bf518df8ca20009d9a0f3419e2
nirhuri <hurinir@gmail.com>
2021-09-03 00:33:03 +0300

    Adding cart screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,33 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
 
-import Product from '../components/Product';
+import Product from "../components/Product";
+import { listProducts } from "../store/actions/productActions";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get("/api/products");
-            setProducts(data);
-        }
-        
-        fetchProducts();
-    }, []);
+  const productList = useSelector((state) => state.productList);
 
-    return (
-        <React.Fragment>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </React.Fragment>
-    )
-}
+  const { loading, error, products } = productList;
 
-export default HomeScreen
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  return (
+    <React.Fragment>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <h3>
+          <Message variant='danger'>{error}</Message>
+        </h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default HomeScreen;
